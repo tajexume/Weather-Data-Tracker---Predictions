5186d79c7e6e6f1e9e8cb65adc6d680adcc04651
Taj Exume <tajexume@gmail.com>
2025-05-10 18:38:25 -0700

    kdkd

--- utils/run_script.ps1
+++ utils/run_script.ps1
@@ -1,5 +1,5 @@
 # Activate virtual environment
-& ".\venv\Scripts\Activate.ps1"
+& ".\.venv\Scripts\Activate.ps1"
 
 # Run the CLI script with desired arguments
-C:\Python312\python.exe -m weather_cli --city "San Jose"
+python -m weather_cli --city "San Jose"
